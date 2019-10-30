2ddf4a73f008dc67c565c75f649faae1ba8378ee
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-10-30 20:32:04 +0900

    feat: cool header

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="header">
+        <h2 className="header-org">INU, 이루고 누리다.</h2>
+        <h1 className="header-title">Bucket List</h1>
+        <p className="header-desc">2020년, 이런 동아리를 만들어 나갈 거예요!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
