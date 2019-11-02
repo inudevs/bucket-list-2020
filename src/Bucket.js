915e8a53a77d80f7261ef50db0f9d82cd50767ac
Woo-sang yun <wsy04018@naver.com>
2019-11-02 20:36:00 +0900

    fix: Apply CSS-in-JS to Bucket.js

--- src/Bucket.js
+++ src/Bucket.js
@@ -1,24 +1,56 @@
 import React from 'react';
-import './styles/Bucket.scss';
+import styled from 'styled-components';
 
 const BucketItem = ({ name, desc, tags }) => (
-  <div className="bucket">
-    <div className="bucket-tags">
+  <Bucket>
+    <Tags>
       {tags.map((tag) => {
         return (
-          <span className="bucket-tag">
+          <Tag>
             { `#${tag}` }
-          </span>
+          </Tag>
         );
       })}
-    </div>
-    <h1 className="bucket-name">
+    </Tags>
+    <Name>
       { name }
-    </h1>
-    <p className="bucket-desc">
+    </Name>
+    <Desc>
       { desc }
-    </p>
-  </div>
+    </Desc>
+  </Bucket>
 );
+
+
+const Bucket = styled.div`
+  flex: 0 0 30%;
+  margin-bottom: 1.5rem;
+  padding-bottom: 0.5rem;
+
+  @media (max-width: 560px) {
+  flex: 0 0 100%;
+`
+
+const Tags = styled.div`
+  margin-bottom: 0.2rem;
+`
+
+const Tag = styled.div`
+  display: inline-block;
+  font-weight: 300;
+`
+
+const Name = styled.h1`
+  margin-bottom: 0.3rem;
+  font-weight: 700;
+  font-size: 1.1rem;
+  word-break: keep-all;
+`
+
+const Desc = styled.p`
+  font-weight: 400;
+  font-size: 0.8rem;
+  word-break: keep-all;
+`
 
 export default BucketItem;
--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-//import './styles/Header.scss';
 import styled from 'styled-components'; 
 
 const Header = () => (
