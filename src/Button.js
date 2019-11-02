e443711ca20ff42858536f25081d1bf333af012b
Woo-sang yun <wsy04018@naver.com>
2019-11-02 20:39:14 +0900

    fix: Apply CSS-in-JS to Button.js

--- src/Button.js
+++ src/Button.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import './styles/Button.scss';
+import styled from 'styled-components'; 
 
 const Button = ({ onClick }) => (
-  <button className="button" onClick={onClick}>
+  <Btn onClick={onClick}>
     함께 이루어 나가기
-  </button>
+  </Btn>
 )
 
+const Btn = styled.button`
+  cursor: pointer;
+  color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  width: fit-content;
+  padding: 0.7rem 1.5rem;
+  border: 1px solid rgba(0, 0, 0, 0.5);
+  border-radius: 25px;
+  font-size: 1.1rem;
+  font-weight: 700;
+  transition: all 0.2s linear;
+  
+  &:focus {
+    outline: none;
+  }
+`
 export default Button;
